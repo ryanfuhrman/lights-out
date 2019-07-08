7e46756a96f00cbf166cc78caf3ea503c26efd0d
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-07-08 14:12:37 -0400

    Updated title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
+        <h1>Lights Out!</h1>
         <Board />
       </div>
     );
